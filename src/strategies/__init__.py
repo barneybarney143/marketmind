1623b725e3a5f64f95c7ae20b84f18a20356e2c8
barneybarney143 <41115329+barneybarney143@users.noreply.github.com>
2025-06-30 22:26:38 +0200

    Add Breakout strategy

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,13 +1,21 @@
 from __future__ import annotations
 
+from .breakout import BreakoutStrategy
 from .ibs import IBSStrategy
 from .macd import MACDStrategy
 from .rsi import RSIStrategy
 
 STRATEGIES = {
+    "breakout": BreakoutStrategy,
     "ibs": IBSStrategy,
     "macd": MACDStrategy,
     "rsi": RSIStrategy,
 }
 
-__all__ = ["IBSStrategy", "MACDStrategy", "RSIStrategy", "STRATEGIES"]
+__all__ = [
+    "BreakoutStrategy",
+    "IBSStrategy",
+    "MACDStrategy",
+    "RSIStrategy",
+    "STRATEGIES",
+]
